5fec36a7b3bca62f5ee3ce18ab0184b36b76ddb4
lucirie <lucirieko@gmail.com>
2024-11-29 17:22:42 +0200

    Added static files, template files, and the hand detector in innovation

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+from cs50 import SQL
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/innovation")
+def innovation():
+    return render_template('innovation.html')
+
---
+++ static/innovation.js
@@ -0,0 +1,59 @@
+window.onload = async function () {
+    webcamElement = document.getElementById('webcam');
+    predictionElement = document.getElementById('prediction');
+
+    canvasElement = document.createElement('canvas');
+    document.body.appendChild(canvasElement);
+    canvasCtx = canvasElement.getContext('2d');
+
+    stream = await navigator.mediaDevices.getUserMedia({ video: true })
+    webcamElement.srcObject = stream;
+    webcamElement.style.display = 'none'; //hide webcam
+
+    hands = new Hands({
+        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
+    });
+
+    hands.setOptions({
+        maxNumHands: 2,
+        modelComplexity: 1,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5,
+    });
+
+    hands.onResults((results) => {
+        canvasCtx.save();
+        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+        // Flip the canvas so it's not upside down
+        canvasElement.style.transform = "rotate(180deg)";
+        
+        canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+        if (results.multiHandLandmarks.length > 0) {
+            for (landmarks of results.multiHandLandmarks) {
+                // Log the landmarks data to the console
+                console.log("This is data coming from your joints. Using this, we can create applications like sign language interperters and even remote control of computers.:", landmarks);
+
+                // Draw the connectors and landmarks
+                drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#FF00FF', lineWidth: 2 });
+                drawLandmarks(canvasCtx, landmarks, { color: '#00FFFF', lineWidth: 1 });
+            }
+        }
+        canvasCtx.restore();
+    });
+
+    camera = new Camera(webcamElement, {
+        onFrame: async () => {
+            await hands.send({ image: webcamElement });
+        },
+        width: 640,
+        height: 480,
+    });
+    camera.start();
+
+    webcamElement.addEventListener('loadeddata', () => {
+        canvasElement.width = webcamElement.videoWidth;
+        canvasElement.height = webcamElement.videoHeight;
+    });
+}
